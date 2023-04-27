ac9220c03c5be08a40c263665d8252c3b5975d23
SHjNEe <baotrung.jpvn@gmail.com>
2023-04-28 00:43:00 +0700

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
