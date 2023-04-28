17665b559fe053f48d6e150fc3d0c72fafc4050f
hapo-trungnb <baotrung.jpvn@gmail.com>
2023-04-29 02:40:18 +0700

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
