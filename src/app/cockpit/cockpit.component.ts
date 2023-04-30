37486e981f898c5938ac553665736274a0fd9056
SHjNEe <baotrung.jpvn@gmail.com>
2023-04-30 15:28:19 +0700

    Add

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -8,14 +8,17 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output('srvCreated') serverCreated = new EventEmitter<{name: string, content: string }>();
   @Output('bpCreated') bluesprintAdded = new EventEmitter<{name: string, content: string }>();
+
+
   newServerName = '';
   newServerContent = '';
   constructor() { }
 
   ngOnInit(): void {
   }
-  onAddServer() {
-    this.serverCreated.emit({name: this.newServerName, content: this.newServerContent});
+  onAddServer(serverName: HTMLInputElement, serverContent: HTMLInputElement) {
+    // this.serverCreated.emit({name: this.newServerName, content: this.newServerContent});
+    this.serverCreated.emit({name: serverName.value, content: serverContent.value});
   }
   onAddBlueprint() {
     this.bluesprintAdded.emit({name: this.newServerName, content: this.newServerContent});
